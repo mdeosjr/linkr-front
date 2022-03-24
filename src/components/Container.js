ac78f3a25b6ebb6237834b5f4c1bc2ee7ed91310
Moises Junior <m.junior@eq.ufrj.br>
2022-03-24 03:25:40 -0300

    feat: layout mobile

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,16 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     display: flex;
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-width: 600px) {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        min-height: 100vh;
+    }
 `;
 
 export default Container;
