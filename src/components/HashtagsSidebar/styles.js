fbb1246fa4ef261125adc4cdf311137fa4af1664
pedro <pedrohperoni@gmail.com>
2022-03-27 00:59:21 -0300

    feat: add hashtagsSidebar

---
+++ src/components/HashtagsSidebar/styles.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const SidebarContainer = styled.div`
+  height: 406px;
+  max-width: 301px;
+  width: 301px;
+  background: #171717;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  margin-left: 25px;
+  @media (max-width: 920px) {
+    display: none;
+  }
+`;
+
+const SidebarTitle = styled.p`
+  font-family: "Oswald";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 27px;
+  line-height: 40px;
+  color: #ffffff;
+  height: 61px;
+  width: 100%;
+  border-bottom: 1px solid #484848;
+  padding: 9px 16px;
+  margin-bottom: 22px;
+`;
+
+const HashtagLink = styled.p`
+  font-family: "Lato";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 19px;
+  line-height: 23px;
+  letter-spacing: 0.05em;
+  color: #ffffff;
+  margin: 3px 16px;
+  cursor: pointer;
+`;
+
+export { SidebarTitle, SidebarContainer, HashtagLink };
