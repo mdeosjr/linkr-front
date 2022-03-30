406607bba9d255df6fa3b886d119233bab4d3f2f
ariel <ariel1242009@gmail.com>
2022-03-29 22:56:00 -0300

    busca priorizada

--- src/services/api.js
+++ src/services/api.js
@@ -42,8 +42,9 @@ async function editPost(postId, token, text) {
   return axios.put(`${BASE_URL}/post/${postId}`, {text}, config);
 }
 
-async function searchUsersByName(name) {
-  return axios.get(`${BASE_URL}/users/search?name=${name}`);
+async function searchUsersByName(token, name) {
+  const config = createConfig(token)
+  return axios.get(`${BASE_URL}/users/search?name=${name}`, config);
 }
 
 async function getTrendingHashtags(token) {
