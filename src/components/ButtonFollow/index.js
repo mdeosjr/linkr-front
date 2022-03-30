41a54c35c062b40d221cfcdda774cba7cb937a3a
Natanna <natannacortez@gmail.com>
2022-03-30 17:20:32 -0300

    fix: solving bugs follow

--- src/components/ButtonFollow/index.js
+++ src/components/ButtonFollow/index.js
@@ -9,27 +9,36 @@ export function ButtonFollow({ followingId }) {
     const [isDisable, setIsDisable] = useState(false);
     const { auth } = useAuth();
 
+
     useEffect(() => {
+        if (!followingId) return;
+
         if (auth) {
             const promise = api.getFollow(auth.token, followingId);
             promise.then((response) => {
-                console.log("response", response.data);
-                setIsFollow(response.data.follow);
-            })
+
+                const following = response.data.follow;
+                setIsFollow(following);
+
+            });
             promise.catch((error) => {
                 alert("erro ao carregar seguidor");
             })
         }
+
     }, []);
+
 
     async function follow(followingId) {
         setIsDisable(true);
         try {
+            console.log("na funçao follow", isFollow);
             if (isFollow === false) {
                 await api.follow(auth.token, followingId);
                 setIsFollow(true);
                 setIsDisable(false);
             }
+
             if (isFollow === true) {
                 await api.unfollow(auth.token, followingId);
                 setIsFollow(false);
